b56d8dc015ed8535e63ef12ac6eee99983fbfd6b
Sajib Biswas <sajibb813@gmail.com>
2021-08-29 21:16:51 +0530

    Complete project

--- Section A/question1.py
+++ Section A/question1.py
@@ -1,2 +1,24 @@
+
 import pandas as pd
-print(pd. __version__ )
+import matplotlib.pyplot as plt
+
+plt.rcParams["figure.figsize"] = [10, 6]
+plt.xlabel("Items")
+plt.ylabel("Sales")
+
+data = pd.read_csv(r"C:\Users\USER\Downloads\BreadBasket_DMS.csv") 
+
+#Now we will firstchange index to item to sort items into new datasets without 'NONE' type of Items.
+items = data.set_index(['Item'])
+NewItems= items.drop(['NONE'])
+NewItems.reset_index(inplace = True)
+
+#after reseting the index, we will count the items and print the frequency of top 10 selling items.
+Top=NewItems['Item'].value_counts()
+print("\nFrequency of top 10 selling items: \n")
+print(Top.head(10))
+
+Top.head(10).plot(kind='bar', color='red')
+plt.show()
+
+
